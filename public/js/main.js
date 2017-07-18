6e907f6a877d708a01035aadb40a78b8dea44920
dmotts <daley.mottley@hotmail.com>
2017-07-18 04:18:20 +0000

    display images

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,24 @@
-$(document).ready (function() {
-    
-    new WOW().init();
-    
-    
-   $('.js-wp-2').waypoint(function(direction) {
-       
-       $('.js-wp-2').addClass('animated slideInUp');
-       
-   }, {
-		offset: '70%'
-	});
-    
-    $('.js-wp-3').waypoint(function(direction) {
-       
-       $('.js-wp-3').addClass('animated fadeIn');
-       
-   }, {
-		offset: '70%'
-	});
-    
-    
-    
-    
-    $(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
-    $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
-    
-    
-    
+new Vue({
+    el: '#vapp',
+    data: {
+        images: [
+            '/img/dogs/dog-1.jpg',
+            '/img/dogs/dog-2.jpg',
+            '/img/dogs/dog-3.jpg',
+            '/img/dogs/dog-4.jpg',
+            '/img/dogs/dog-5.jpg',
+            '/img/dogs/dog-6.jpg',
+            '/img/dogs/dog-7.jpg',
+            '/img/dogs/dog-8.jpg',
+            '/img/dogs/dog-9.jpg',
+            '/img/dogs/dog-10.jpg',
+            '/img/dogs/dog-11.jpg',
+            '/img/dogs/dog-12.jpg',
+            '/img/dogs/dog-13.jpg',
+            '/img/dogs/dog-14.jpg',
+            '/img/dogs/dog-15.jpg',
+            '/img/dogs/dog-16.jpg'
+            ]
+    },
     
 });
